5d8ee232783609fe15d7003af42176c1d3665725
tommyd2377 <tomdevito27@gmail.com>
2022-09-18 20:24:10 +0000

    createPool

--- programs/pvp-amm/src/lib.rs
+++ programs/pvp-amm/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+//use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer, MintTo};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,10 +7,47 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod pvp_amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_pool(ctx: Context<CreatePool>, amount: u64) -> Result<()> {
+        let poolData: &mut Account<Pool> = &mut ctx.accounts.pool;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get().unwrap();
+
+        poolData.author = *author.key;
+        poolData.timestamp = clock.unix_timestamp;
+        poolData.amount = amount;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreatePool<'info> {
+    #[account(init, payer = author, space = Pool::LEN)]
+    pub pool: Account<'info, Pool>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Pool {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub amount: u64,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars max.
+const MAX_CONTENT_LENGTH: usize = 280 * 4; // 280 chars max.
+const REVIEW_LENGTH: usize = 32;
+
+impl Pool {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH
+        + REVIEW_LENGTH; // Content.
+}
--- tests/pvp-amm.ts
+++ tests/pvp-amm.ts
@@ -1,6 +1,10 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { PvpAmm } from "../target/types/pvp_amm";
+import * as assert from "assert";
+import * as bs58 from "bs58";
+import * as serumCmn from "@project-serum/common";
+import { TokenInstructions } from  "@project-serum/serum";
 
 describe("pvp-amm", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +12,126 @@ describe("pvp-amm", () => {
 
   const program = anchor.workspace.PvpAmm as Program<PvpAmm>;
 
+  let usdcMint = null;
+  let gdTokenMint = null;  
+  let poolusdc = null;
+  let longusdc = null;
+  let shortusdc = null;
+  let poolgd = null;
+  let longgd = null;
+  let shortgd = null;
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+   //usdcMint = await spl.Token.createMint(program.provider.connection, program.provider.wallet.publicKey, program.provider.wallet.publicKey, program.provider.wallet.publicKey, 0, program.programId);
+    gdTokenMint = await createMint(program.provider);
+    // poolusdc = await createTokenAccount(program.provider, usdcMint, program.provider.wallet.publicKey);
+    // longusdc = await createTokenAccount(program.provider, usdcMint, program.provider.wallet.publicKey);
+    // shortusdc = await createTokenAccount(program.provider, usdcMint, program.provider.wallet.publicKey);
+    // poolgd = await createTokenAccount(program.provider, gdTokenMint, program.provider.wallet.publicKey);
+    // longgd = await createTokenAccount(program.provider, gdTokenMint, program.provider.wallet.publicKey);
+    // shortgd = await createTokenAccount(program.provider, gdTokenMint, program.provider.wallet.publicKey);
+
+  });
+
+  it("Can create a new pool", async () => {
+    // Add your test here.   
+    let pool = anchor.web3.Keypair.generate();
+    const tx = await program.rpc.createPool(new anchor.BN(100), {
+        accounts: {
+            pool: pool.publicKey,
+            author: program.provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [pool],
+    });
     console.log("Your transaction signature", tx);
   });
 });
+
+// const program = anchor.workspace.PvpAmm as Program<PvpAmm>;
+
+const TOKEN_PROGRAM_ID = new anchor.web3.PublicKey(
+  TokenInstructions.TOKEN_PROGRAM_ID.toString()
+);
+
+async function getTokenAccount(provider, addr) {
+  return await serumCmn.getTokenAccount(provider, addr);
+}
+
+async function getMintInfo(provider, mintAddr) {
+  return await serumCmn.getMintInfo(provider, mintAddr);
+}
+
+async function createMint(provider, authority) {
+  if (authority === undefined) {
+    authority = provider.wallet.publicKey;
+  }
+  const mint = anchor.web3.Keypair.generate();
+  const instructions = await createMintInstructions(
+    provider,
+    authority,
+    mint.publicKey
+  );
+
+  const tx = new anchor.web3.Transaction();
+  tx.add(...instructions);
+
+  await provider.send(tx, [mint]);
+
+  return mint.publicKey;
+}
+
+async function createMintInstructions(provider, authority, mint) {
+  let instructions = [
+    anchor.web3.SystemProgram.createAccount({
+      fromPubkey: provider.wallet.publicKey,
+      newAccountPubkey: mint,
+      space: 82,
+      lamports: await provider.connection.getMinimumBalanceForRentExemption(82),
+      programId: TOKEN_PROGRAM_ID,
+    }),
+    TokenInstructions.initializeMint({
+      mint,
+      decimals: 9,
+      mintAuthority: authority,
+    }),
+  ];
+  return instructions;
+}
+
+async function createTokenAccount(provider, mint, owner) {
+  const vault = anchor.web3.Keypair.generate();
+  const tx = new anchor.web3.Transaction();
+  tx.add(
+    ...(await createTokenAccountInstrs(program.provider, vault.publicKey, mint, owner))
+  );
+  await provider.send(tx, [vault]);
+  return vault.publicKey;
+}
+
+async function createTokenAccountInstrs(
+  provider,
+  newAccountPubkey,
+  mint,
+  owner,
+  lamports
+) {
+  if (lamports === undefined) {
+    lamports = await provider.connection.getMinimumBalanceForRentExemption(165);
+  }
+  return [
+    anchor.web3.SystemProgram.createAccount({
+      fromPubkey: provider.wallet.publicKey,
+      newAccountPubkey,
+      space: 165,
+      lamports,
+      programId: TOKEN_PROGRAM_ID,
+    }),
+    TokenInstructions.initializeAccount({
+      account: newAccountPubkey,
+      mint,
+      owner,
+    }),
+  ];
+}
