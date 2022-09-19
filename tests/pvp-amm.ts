c4c4c308f8766ec91c0ded5698d0af5b7c225e0f
tommyd2377 <tomdevito27@gmail.com>
2022-09-19 16:20:06 +0000

    mints and token accounts

--- tests/pvp-amm.ts
+++ tests/pvp-amm.ts
@@ -5,6 +5,8 @@ import * as assert from "assert";
 import * as bs58 from "bs58";
 import * as serumCmn from "@project-serum/common";
 import { TokenInstructions } from  "@project-serum/serum";
+import * as Spl from "@solana/spl-token";
+import { PublicKey } from "@solana/web3.js";
 
 describe("pvp-amm", () => {
   // Configure the client to use the local cluster.
@@ -12,25 +14,108 @@ describe("pvp-amm", () => {
 
   const program = anchor.workspace.PvpAmm as Program<PvpAmm>;
 
+  let usdcKeypair = null;
   let usdcMint = null;
+  let gdTokenKeypair = null;
   let gdTokenMint = null;  
+  let poolKeypair = null;
   let poolusdc = null;
+  let poolgd = null;
+  let longKeypair = null;
+  let shortKeypair = null;
   let longusdc = null;
   let shortusdc = null;
-  let poolgd = null;
   let longgd = null;
   let shortgd = null;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-   //usdcMint = await spl.Token.createMint(program.provider.connection, program.provider.wallet.publicKey, program.provider.wallet.publicKey, program.provider.wallet.publicKey, 0, program.programId);
-    gdTokenMint = await createMint(program.provider);
-    // poolusdc = await createTokenAccount(program.provider, usdcMint, program.provider.wallet.publicKey);
-    // longusdc = await createTokenAccount(program.provider, usdcMint, program.provider.wallet.publicKey);
-    // shortusdc = await createTokenAccount(program.provider, usdcMint, program.provider.wallet.publicKey);
-    // poolgd = await createTokenAccount(program.provider, gdTokenMint, program.provider.wallet.publicKey);
+    usdcKeypair = await anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(usdcKeypair.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature);
+    
+    usdcMint = await Spl.createMint(program.provider.connection, 
+        usdcKeypair,           
+        usdcKeypair.publicKey,       
+        null,                         
+        6);
+    console.log("Created USDC Mint: " + usdcMint.toBase58());
+
+    gdTokenKeypair = await anchor.web3.Keypair.generate();
+    const signature1 = await program.provider.connection.requestAirdrop(gdTokenKeypair.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature1);
+    
+    gdTokenMint = await Spl.createMint(
+        program.provider.connection, 
+        gdTokenKeypair,  
+        gdTokenKeypair.publicKey,          
+        null,                          
+        12);
+    console.log("Created GD Token Mint: " + gdTokenMint.toBase58());
+
+    poolKeypair = await anchor.web3.Keypair.generate();
+    const signature2 = await program.provider.connection.requestAirdrop(poolKeypair.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature2);
+
+    const usdcMintPublicKey = new PublicKey(usdcMint);
+    const gdTokenMintPublicKey = new PublicKey(gdTokenMint);
+
+    poolusdc = await Spl.createAssociatedTokenAccount(
+        program.provider.connection, 
+        poolKeypair,  
+        usdcMintPublicKey,
+        poolKeypair.publicKey
+    );
+    console.log("Created USDC Pool: " + poolusdc.toBase58());
+
+    poolgd = await Spl.createAssociatedTokenAccount(
+        program.provider.connection, 
+        poolKeypair,  
+        gdTokenMintPublicKey,
+        poolKeypair.publicKey
+    );
+    console.log("Created GD Token Pool: " + poolgd.toBase58());
+
+    longKeypair = await anchor.web3.Keypair.generate();
+    const signature3 = await program.provider.connection.requestAirdrop(longKeypair.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature3);
+    
+    longusdc = await Spl.createAssociatedTokenAccount(
+        program.provider.connection, 
+        longKeypair,  
+        usdcMintPublicKey,
+        longKeypair.publicKey
+    );
+    console.log("Created Long USDC ATA: " + longusdc.toBase58());
+
+    shortKeypair = await anchor.web3.Keypair.generate();
+    const signature4 = await program.provider.connection.requestAirdrop(shortKeypair.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature4);
+    
+    shortusdc = await Spl.createAssociatedTokenAccount(
+        program.provider.connection, 
+        shortKeypair,  
+        usdcMintPublicKey,
+        shortKeypair.publicKey
+    );
+    console.log("Created Short USDC ATA: " + shortusdc.toBase58());
+
     // longgd = await createTokenAccount(program.provider, gdTokenMint, program.provider.wallet.publicKey);
+    longgd = await Spl.createAssociatedTokenAccount(
+        program.provider.connection, 
+        longKeypair,
+        gdTokenMintPublicKey,
+        longKeypair.publicKey
+    );
+    console.log("Created Long GD Token ATA: " + longgd.toBase58());
+
     // shortgd = await createTokenAccount(program.provider, gdTokenMint, program.provider.wallet.publicKey);
+    shortgd = await Spl.createAssociatedTokenAccount(
+        program.provider.connection, 
+        shortKeypair,  
+        gdTokenMintPublicKey,
+        shortKeypair.publicKey
+    );
+    console.log("Created Long USDC ATA: " + shortgd.toBase58());
 
   });
 
@@ -48,90 +133,3 @@ describe("pvp-amm", () => {
     console.log("Your transaction signature", tx);
   });
 });
-
-// const program = anchor.workspace.PvpAmm as Program<PvpAmm>;
-
-const TOKEN_PROGRAM_ID = new anchor.web3.PublicKey(
-  TokenInstructions.TOKEN_PROGRAM_ID.toString()
-);
-
-async function getTokenAccount(provider, addr) {
-  return await serumCmn.getTokenAccount(provider, addr);
-}
-
-async function getMintInfo(provider, mintAddr) {
-  return await serumCmn.getMintInfo(provider, mintAddr);
-}
-
-async function createMint(provider, authority) {
-  if (authority === undefined) {
-    authority = provider.wallet.publicKey;
-  }
-  const mint = anchor.web3.Keypair.generate();
-  const instructions = await createMintInstructions(
-    provider,
-    authority,
-    mint.publicKey
-  );
-
-  const tx = new anchor.web3.Transaction();
-  tx.add(...instructions);
-
-  await provider.send(tx, [mint]);
-
-  return mint.publicKey;
-}
-
-async function createMintInstructions(provider, authority, mint) {
-  let instructions = [
-    anchor.web3.SystemProgram.createAccount({
-      fromPubkey: provider.wallet.publicKey,
-      newAccountPubkey: mint,
-      space: 82,
-      lamports: await provider.connection.getMinimumBalanceForRentExemption(82),
-      programId: TOKEN_PROGRAM_ID,
-    }),
-    TokenInstructions.initializeMint({
-      mint,
-      decimals: 9,
-      mintAuthority: authority,
-    }),
-  ];
-  return instructions;
-}
-
-async function createTokenAccount(provider, mint, owner) {
-  const vault = anchor.web3.Keypair.generate();
-  const tx = new anchor.web3.Transaction();
-  tx.add(
-    ...(await createTokenAccountInstrs(program.provider, vault.publicKey, mint, owner))
-  );
-  await provider.send(tx, [vault]);
-  return vault.publicKey;
-}
-
-async function createTokenAccountInstrs(
-  provider,
-  newAccountPubkey,
-  mint,
-  owner,
-  lamports
-) {
-  if (lamports === undefined) {
-    lamports = await provider.connection.getMinimumBalanceForRentExemption(165);
-  }
-  return [
-    anchor.web3.SystemProgram.createAccount({
-      fromPubkey: provider.wallet.publicKey,
-      newAccountPubkey,
-      space: 165,
-      lamports,
-      programId: TOKEN_PROGRAM_ID,
-    }),
-    TokenInstructions.initializeAccount({
-      account: newAccountPubkey,
-      mint,
-      owner,
-    }),
-  ];
-}
